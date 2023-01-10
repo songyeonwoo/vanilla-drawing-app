4e11638cf53e0759b0d36f61f837d60cec5cc4ba
songyeonwoo <91600011+songyeonwoo@users.noreply.github.com>
2023-01-10 15:40:24 +0900

    Add files via upload
    const, let, var

---
+++ app1.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "nico";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "nicolas";
+
+console.log("your new name is " + myName);
